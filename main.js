86483f5f221947b3a99ecd90ffffa7cced64a345
anmoldhanda <rk352714@gmail.com>
2023-05-20 13:11:43 +0530

    functions updated & theme

---
+++ main.js
@@ -0,0 +1,31 @@
+const sidebar = document.querySelector("aside .sidebar");
+const openbtn = document.getElementById("openbtn");
+const closebtn = document.getElementById("closebtn");
+const lightmode = document.getElementById("lightmode");
+const darkmode = document.getElementById("darkmode");
+
+openbtn.addEventListener("click", function showsidebar() {
+  sidebar.style.display = "block";
+  openbtn.style.display = "none";
+  closebtn.style.display = "block";
+  openbtn.classList.add("activeopenmenu");
+}); 
+
+closebtn.addEventListener("click", function hidesidebar() {
+  sidebar.style.display = "none";
+  openbtn.style.display = "block";
+  closebtn.style.display = "none";
+  openbtn.classList.remove("activeopenmenu");
+});
+
+darkmode.addEventListener("click",function myfunction(){
+document.body.classList.add("dark-theme-variables");
+darkmode.classList.add("current-theme");
+lightmode.classList.remove("current-theme");
+})  
+
+lightmode.addEventListener("click", function myfunction(){
+  document.body.classList.remove("dark-theme-variables");
+lightmode.classList.add("current-theme");
+darkmode.classList.remove("current-theme");
+})
